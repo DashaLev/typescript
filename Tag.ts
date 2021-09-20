5b5160cd245f396d9832661fc853af06d3d0ea1b
Dasha <trofimova.dashas@gmail.com>
2021-09-20 11:03:04 +0300

    added typescript files

---
+++ Tag.ts
@@ -0,0 +1,8 @@
+export interface ITag {
+    tagName: string;
+    actionsDescription: string;
+    attributeName1: string;
+    attributeDescription1: string;
+    attributeName2: string;
+    attributeDescription2: string;
+}
---
+++ main_2.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var post = {
+    id: 1,
+    title: 'Hello world',
+    userId: 2,
+    body: 'dfsjfjbfjb bkjbkjwbcbjkb kjbjk bkbjk'
+};
+var user = {
+    id: 1,
+    name: 'Kim',
+    username: 'Kim Qwo',
+    email: '123@email.com',
+    phone: '+380123456789',
+    website: '123.com.ua',
+    company: { name: 'Hello', catchPhrase: 'Hi', bs: 'ok' },
+    address: { city: 'Kiev', street: 'Main', suite: '12', zipcode: '01231', geo: { lat: '34.1', lng: '25.1' } }
+};
